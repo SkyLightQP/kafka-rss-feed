3d80cfb3ea475cfa9125eb054a84b308150ff035
Daegyeom Ha <combbm@gmail.com>
2024-12-01 15:19:49 +0900

    feat: implement `FetchRssService`

--- modules/rss-producer/src/main/kotlin/me/daegyeo/kafkarssfeed/rssproducer/application/port/in/FetchRssUseCase.kt
+++ modules/rss-producer/src/main/kotlin/me/daegyeo/kafkarssfeed/rssproducer/application/port/in/FetchRssUseCase.kt
@@ -1,5 +1,7 @@
 package me.daegyeo.kafkarssfeed.rssproducer.application.port.`in`
 
+import me.daegyeo.kafkarssfeed.rssproducer.application.domain.RssArticle
+
 fun interface FetchRssUseCase {
-    fun fetchRss(url: String)
+    fun fetchRss(url: String): List<RssArticle>
 }
--- modules/rss-producer/src/main/kotlin/me/daegyeo/kafkarssfeed/rssproducer/application/service/FetchRssService.kt
+++ modules/rss-producer/src/main/kotlin/me/daegyeo/kafkarssfeed/rssproducer/application/service/FetchRssService.kt
@@ -1,11 +1,36 @@
 package me.daegyeo.kafkarssfeed.rssproducer.application.service
 
+import me.daegyeo.kafkarssfeed.rssproducer.application.domain.RssArticle
 import me.daegyeo.kafkarssfeed.rssproducer.application.port.`in`.FetchRssUseCase
+import org.jsoup.Jsoup
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 @Service
 class FetchRssService : FetchRssUseCase {
-        // TODO
-    override fun fetchRss(url: String) {
+    override fun fetchRss(url: String): List<RssArticle> {
+        val res = Jsoup.connect(url).get()
+        val feed = res.select("channel > item")
+        return feed.map {
+            val title = it.select("title").text()
+            val link = it.select("link").text()
+            val description = it.select("description").text()
+            val rawDate = it.select("pubDate").text()
+            val date = LocalDateTime.parse(rawDate, DateTimeFormatter.RFC_1123_DATE_TIME).atZone(ZoneId.of("GMT"))
+
+            makeRssArticle(title, link, description, date)
+        }
+    }
+
+    private fun makeRssArticle(title: String, link: String, description: String, date: ZonedDateTime): RssArticle {
+        return RssArticle(
+            title = title,
+            link = link,
+            description = description,
+            date = date
+        )
     }
 }
