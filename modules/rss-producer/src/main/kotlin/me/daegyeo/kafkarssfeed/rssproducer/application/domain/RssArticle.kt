0e467280ddbdd7ab3923ffb0e3570de955234e37
Daegyeom Ha <combbm@gmail.com>
2024-12-01 15:14:14 +0900

    fix: change type of `date` to `ZonedDateTime`

--- modules/rss-producer/src/main/kotlin/me/daegyeo/kafkarssfeed/rssproducer/application/domain/RssArticle.kt
+++ modules/rss-producer/src/main/kotlin/me/daegyeo/kafkarssfeed/rssproducer/application/domain/RssArticle.kt
@@ -1,8 +1,10 @@
 package me.daegyeo.kafkarssfeed.rssproducer.application.domain
 
+import java.time.ZonedDateTime
+
 data class RssArticle(
     val title: String,
     val link: String,
     val description: String,
-    val date: String
+    val date: ZonedDateTime
 )
