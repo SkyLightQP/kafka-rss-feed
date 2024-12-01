4b025abca434e2a78c06c588a324c97d48db8f93
Daegyeom Ha <combbm@gmail.com>
2024-12-01 15:08:32 +0900

    chore: add `jsoup` dependency

--- modules/rss-producer/build.gradle.kts
+++ modules/rss-producer/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.kafka:spring-kafka")
+    implementation("org.jsoup:jsoup:1.18.2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.kafka:spring-kafka-test")
 }
