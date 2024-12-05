da9eab0379d36f8736a9d1cc591cd9e5386a21e7
Daegyeom Ha <combbm@gmail.com>
2024-12-05 18:50:45 +0900

    feat: create `KafkaConsumerConfig`

---
+++ modules/consumer/src/main/kotlin/me/daegyeo/kafkarssfeed/consumer/config/KafkaConsumerConfig.kt
@@ -0,0 +1,41 @@
+package me.daegyeo.kafkarssfeed.rssproducer.config
+
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.annotation.EnableKafka
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.ConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.support.serializer.JsonSerializer
+
+
+@EnableKafka
+@Configuration
+class KafkaConsumerConfig {
+    @Value("\${spring.kafka.bootstrap-servers}")
+    lateinit var bootstrapServers: String
+
+    @Value("\${spring.kafka.consumer.group-id}")
+    lateinit var groupId: String
+
+    @Bean
+    fun consumerFactory(): ConsumerFactory<String, Any> {
+        val configProps = mapOf<String, Any>(
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
+            ConsumerConfig.GROUP_ID_CONFIG to groupId,
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to JsonSerializer::class.java
+        )
+        return DefaultKafkaConsumerFactory(configProps)
+    }
+
+    @Bean
+    fun kafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, Any> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, Any>()
+        factory.consumerFactory = consumerFactory()
+        return factory
+    }
+}
