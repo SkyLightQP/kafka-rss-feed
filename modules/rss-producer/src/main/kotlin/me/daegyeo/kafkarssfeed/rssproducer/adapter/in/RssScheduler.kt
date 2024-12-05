9f455100493e9c0309e17807a921daddada46dc7
Daegyeom Ha <combbm@gmail.com>
2024-12-05 17:54:15 +0900

    feat: fetch rss and send data to kafka using `RssResultProcessUseCase`

--- modules/rss-producer/src/main/kotlin/me/daegyeo/kafkarssfeed/rssproducer/adapter/in/RssScheduler.kt
+++ modules/rss-producer/src/main/kotlin/me/daegyeo/kafkarssfeed/rssproducer/adapter/in/RssScheduler.kt
@@ -1,16 +1,21 @@
 package me.daegyeo.kafkarssfeed.rssproducer.adapter.`in`
 
 import me.daegyeo.kafkarssfeed.rssproducer.application.port.`in`.FetchRssUseCase
+import me.daegyeo.kafkarssfeed.rssproducer.application.port.`in`.RssResultProcessUseCase
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 
 @Component
-class RssScheduler(private val fetchRssUseCase: FetchRssUseCase, ) {
+class RssScheduler(
+    private val fetchRssUseCase: FetchRssUseCase,
+    private val rssResultProcessUseCase: RssResultProcessUseCase
+) {
     val RSS_URL = "https://www.korea.kr/rss/policy.xml"
 
     @Scheduled(cron = "0 0 0/1 * * ?")
     fun fetchRss() {
-        fetchRssUseCase.fetchRss(RSS_URL)
+        val result = fetchRssUseCase.fetchRss(RSS_URL)
+        rssResultProcessUseCase.process(result)
     }
 }
