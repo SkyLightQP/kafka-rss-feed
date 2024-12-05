c0a4f6e98974544dfbf06fbb6ea3d1cd961842ac
Daegyeom Ha <combbm@gmail.com>
2024-12-05 19:54:26 +0900

    feat: add `JavaTimeModule` object mapper for `ZonedTimeDate` type

---
+++ modules/consumer/src/main/kotlin/me/daegyeo/kafkarssfeed/consumer/config/ObjectMapperConfig.kt
@@ -0,0 +1,17 @@
+package me.daegyeo.kafkarssfeed.consumer.config
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class ObjectMapperConfig {
+
+    @Bean
+    fun objectMapper(): ObjectMapper {
+        return ObjectMapper().apply {
+            registerModule(JavaTimeModule())
+        }
+    }
+}
