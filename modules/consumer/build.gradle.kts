543487916871a75ee27790d283af58cf87733fa5
Daegyeom Ha <combbm@gmail.com>
2024-12-08 20:06:40 +0900

    chore: add dependencies about JPA and PostgreSQL

--- modules/consumer/build.gradle.kts
+++ modules/consumer/build.gradle.kts
@@ -4,8 +4,10 @@ plugins {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.kafka:spring-kafka")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+    runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.kafka:spring-kafka-test")
 }
