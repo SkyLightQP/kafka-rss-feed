2cc6b3118cf74701d15c5094538c05a2f3da6ec3
Daegyeom Ha <combbm@gmail.com>
2024-12-09 21:11:04 +0900

    fix(consumer): move column of entity to constructor

--- modules/consumer/src/main/kotlin/me/daegyeo/kafkarssfeed/consumer/application/persistence/RssArticleEntity.kt
+++ modules/consumer/src/main/kotlin/me/daegyeo/kafkarssfeed/consumer/application/persistence/RssArticleEntity.kt
@@ -7,24 +7,19 @@ import java.time.ZonedDateTime
 @Entity
 @Table(name = "rss_articles")
 class RssArticleEntity(
-    title: String,
-    link: String,
-    description: String,
-    date: ZonedDateTime
-) {
+    @Column(nullable = false)
+    val title: String,
+
+    @Column(nullable = false)
+    val link: String,
+
+    @Column(nullable = false)
+    val description: String,
+
+    @Column(nullable = false)
+    val date: ZonedDateTime,
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Long = 0
-
-    @Column(nullable = false)
-    var title: String = title
-
-    @Column(nullable = false)
-    var link: String = link
-
-    @Column(nullable = false)
-    var description: String = description
-
-    @Column(nullable = false)
-    var date: ZonedDateTime = date
-}
+    val id: Long? = null
+)
