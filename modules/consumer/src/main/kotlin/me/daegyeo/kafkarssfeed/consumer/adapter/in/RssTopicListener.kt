7a4bc591a6c62a4551b1048c27a47b06aa3afed6
Daegyeom Ha <combbm@gmail.com>
2024-12-09 21:11:45 +0900

    feat(consumer): implement `RssTopicListener`

--- modules/consumer/src/main/kotlin/me/daegyeo/kafkarssfeed/consumer/adapter/in/RssTopicListener.kt
+++ modules/consumer/src/main/kotlin/me/daegyeo/kafkarssfeed/consumer/adapter/in/RssTopicListener.kt
@@ -1,14 +1,15 @@
 package me.daegyeo.kafkarssfeed.consumer.adapter.`in`
 
+import me.daegyeo.kafkarssfeed.consumer.application.port.`in`.SaveManyRssArticleUseCase
 import me.daegyeo.kafkarssfeed.rssproducer.application.domain.RssArticle
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
 
 @Component
-class RssTopicListener {
+class RssTopicListener(private val saveManyRssArticleUseCase: SaveManyRssArticleUseCase) {
 
     @KafkaListener(topics = ["kafka-rss-feed.rss"], groupId = "rss-feed-consumer-group")
     fun listen(message: RssArticle) {
-        println("Received Message: $message")
+        saveManyRssArticleUseCase.saveManyRssArticle(listOf(message))
     }
 }
