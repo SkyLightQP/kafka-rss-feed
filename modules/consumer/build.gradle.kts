4d742007dd59db128fdc05606567a23b9af43bd2
Daegyeom Ha <combbm@gmail.com>
2024-12-13 13:38:59 +0900

    chore(consumer): add `spring-boot-starter-web` dependency

--- modules/consumer/build.gradle.kts
+++ modules/consumer/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.kafka:spring-kafka")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
     runtimeOnly("org.postgresql:postgresql")
