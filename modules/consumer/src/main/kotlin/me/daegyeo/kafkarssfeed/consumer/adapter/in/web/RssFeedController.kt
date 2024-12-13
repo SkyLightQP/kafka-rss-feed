cdee13e3c5681c6474b5f032a7d6e18a87c5754d
Daegyeom Ha <combbm@gmail.com>
2024-12-13 14:07:19 +0900

    feat(consumer) add CORS config

--- modules/consumer/src/main/kotlin/me/daegyeo/kafkarssfeed/consumer/adapter/in/web/RssFeedController.kt
+++ modules/consumer/src/main/kotlin/me/daegyeo/kafkarssfeed/consumer/adapter/in/web/RssFeedController.kt
@@ -3,13 +3,16 @@ package me.daegyeo.kafkarssfeed.consumer.adapter.`in`.web
 import me.daegyeo.kafkarssfeed.consumer.application.port.`in`.ReadAllRssArticleUseCase
 import me.daegyeo.kafkarssfeed.rssproducer.application.domain.RssArticle
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 
 @Controller
 @RestController
 @RequestMapping("/rss")
+@CrossOrigin(origins = ["*"], methods = [RequestMethod.GET])
 class RssFeedController(private val readAllRssArticleUseCase: ReadAllRssArticleUseCase) {
 
     @GetMapping
